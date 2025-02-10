02ddc9612f473fe41efacfa3b189a4c7fb7ab2f6
Youssef Nassar <youssef.nassar@solactive.com>
2025-02-10 12:47:20 +0100

    show response in ClaudeRecipe component

--- ai.js
+++ ai.js
@@ -40,7 +40,7 @@ You are an assistant that receives a list of ingredients that a user has and sug
 
 // Make sure you set an environment variable in Scrimba
 // for HF_ACCESS_TOKEN
-const hf = new HfInference(import.meta.env.AI_TOKEN)
+const hf = new HfInference(import.meta.env.VITE_AI_TOKEN)
 
 export async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
